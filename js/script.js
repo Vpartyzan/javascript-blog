192b3c4672fb9be6091cba15652749862a5a1280
Maks <peregrim.mt@gmail.com>
2021-01-17 14:51:31 +0100

    Add logic to select active articles for script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const titleClickHandler = function(event){
+  event.preventDefault();
+
   const clickedElement = this;
-  console.log('Link was clicked!');
-  console.log('clickedElement (with plus): ' + clickedElement);
 
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -12,7 +12,7 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
@@ -22,11 +22,15 @@ const titleClickHandler = function(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
+
 }
   
 const links = document.querySelectorAll('.titles a');
